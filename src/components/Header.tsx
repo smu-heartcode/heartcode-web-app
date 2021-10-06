cfab6a6430303aa7be81da84ba6ab66bf417a0bc
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2021-10-06 20:02:21 +0800

    feat: add navbar (#5)
    * feat: add tailwindcss to project
    
    * fix: css import path
    
    * style: add poppins font
    
    * feat: add navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,53 @@
-import React from "react";
-import NavLink from "./NavLink";
+import React, { useState } from "react";
+import Link from "next/link";
+
+import Logo from "../assets/Logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <header>
-      <NavLink href="/" label="SMU Heartcode" />
-      <nav>
-        <NavLink href="/partners" label="Partners" />
-        <NavLink href="/past" label="Past Runs" /> 
-      </nav>
-    </header>
+    <>
+      <header className="flex items-center py-2 px-10 flex-wrap bg-white text-black">
+        <div
+          className="lg:text-xl py-2 inline-flex items-center font-bold"
+        >
+          <Link href="/">
+            <img className="h-12" src={Logo.src} alt="SMU Project Heartcode" />
+          </Link>
+        </div>
+        <button
+          onClick={() => setShowNav(!showNav)}
+          type="button"
+          className="inline-flex p-3 text-black rounded-md transition duration-400 ease-in-out hover:bg-gray-300 lg:hidden ml-auto"
+        >
+          <GiHamburgerMenu />
+        </button>
+        <div className="w-full flex-grow lg:inline-flex lg:flex-grow lg:w-auto">
+          <div
+            className={
+              "lg:inline-flex lg:flex-row lg:ml-auto flex flex-col " +
+              (showNav ? "" : "hidden")
+            }
+          >
+            <Link href="/partners">
+              <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded transition duration-400 ease-in-out hover:bg-gray-300">
+                Our Partners
+              </a>
+            </Link>
+
+            <Link href="/year">
+              <a className="lg:inline-flex lg:w-auto px-3 py-2 rounded transition duration-400 ease-in-out hover:bg-gray-300">
+                Previous Runs
+              </a>
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 

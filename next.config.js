92a380a746344a8add478f4897b02618e68f36e2
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2021-10-06 16:48:12 +0800

    feat: add page routing (#3)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, 
+      fs: false, 
+    };
+    return config;
+  },
   reactStrictMode: true,
-}
+};
---
+++ src/components/Years/yearData.ts
@@ -0,0 +1,16 @@
+const yearData = [
+  {
+    "year": "2020",
+    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+  },
+  {
+    "year": "2019",
+    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+  },
+  {
+    "year": "2018",
+    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+  },
+]
+
+export default yearData;

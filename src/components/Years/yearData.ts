748d8e1aa7f7703b6fd67afacd4f4994c67d5edf
seanisoverhere <sean_choon@hotmail.com>
2021-12-14 22:57:04 +0800

    feat: add static site generation for past year runs

--- src/components/Years/yearData.ts
+++ src/components/Years/yearData.ts
@@ -1,16 +1,409 @@
-const yearData = [
-  {
-    "year": "2020",
-    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+interface OrgCommItems {
+  name: string;
+  role: string;
+  image: string;
+}
+
+interface ProjectItems {
+  mentee: string;
+  mentor: string;
+  url: string;
+  thumbnail: string;
+}
+
+interface YearMap {
+  [year: string]: {
+    description: string;
+    orgComm: Array<OrgCommItems>;
+    projects: Array<ProjectItems>;
+  };
+}
+
+const yearData: YearMap = {
+  "2020": {
+    description:
+      "HeartCode 2020 was a virtual run focusing on Digital Content Creation and Web Development",
+    orgComm: [
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+    ],
+    projects: [
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+    ],
   },
-  {
-    "year": "2019",
-    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+  "2019": {
+    description:
+      "HeartCode 2019 focuses on Internet of Things and Web Development",
+    orgComm: [
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+    ],
+    projects: [
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+    ],
   },
-  {
-    "year": "2018",
-    "orgComm": ["Person 1", "Person 2", "Person 3", "Person 4"],
+  "2018": {
+    description:
+      "HeartCode 2018 focuses on Internet of Things and Web Development",
+    orgComm: [
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+      {
+        name: "Sean Choon",
+        role: "Web Developer",
+        image: "",
+      },
+    ],
+    projects: [
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+      {
+        mentee: "Hello World",
+        mentor: "Bye World",
+        url: "",
+        thumbnail: "",
+      },
+    ],
   },
-]
+};
 
 export default yearData;

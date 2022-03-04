94a563e1b6d4ae9138d58108e6d706383f758865
seanisoverhere <sean_choon@hotmail.com>
2022-03-04 18:09:38 +0800

    fix: deployment warnings

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -4,10 +4,23 @@ import useEmblaCarousel from "embla-carousel-react";
 import { PrevButton, NextButton } from "./EmblaCarouselButton";
 import Autoplay from "embla-carousel-autoplay";
 
-import { mediaByIndex } from "./mediaByIndex";
+import hc1 from "/public/static/hc1.jpg";
+import hc2 from "/public/static/hc2.jpg";
+import hc3 from "/public/static/hc3.jpg";
+import hc4 from "/public/static/hc4.jpg";
+import hc5 from "/public/static/hc5.png";
+import hc6 from "/public/static/hc6.jpg";
+import hc7 from "/public/static/hc7.png";
+import hc8 from "/public/static/hc8.jpg";
+import hc9 from "/public/static/hc9.jpg";
+import hc10 from "/public/static/hc10.jpg";
+
 import styles from "./Carousel.module.css";
 
 interface CarouselProps {}
+
+const media = [hc1, hc2, hc3, hc4, hc5, hc6, hc7, hc8, hc9, hc10];
+const mediaByIndex = (index: number) => media[index % media.length];
 
 const SLIDE_COUNT = 10;
 const slides = Array.from(Array(SLIDE_COUNT).keys());
